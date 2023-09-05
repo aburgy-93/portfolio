d3abe7d3ce829a7678c9dc9b092d74d731bdaec1
aburgy-93 <aburgy@uwalumni.com>
2023-09-04 19:04:35 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,31 @@
+const projectSection = document.querySelector(".project");
+const nav = document.querySelector(".navBar");
+
+const stickyNav = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  const { isIntersecting } = entry;
+  console.log(isIntersecting);
+
+  if (!isIntersecting) nav.classList.remove("sticky");
+  else nav.classList.add("sticky");
+};
+
+const projectsObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-150px",
+});
+
+projectsObserver.observe(projectSection);
+
+document.querySelector(".links-right").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains("nav_link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
